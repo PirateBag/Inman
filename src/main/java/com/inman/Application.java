0765577945b4c163cbfd8b1213684a877b8eafe4
Michael Bagley <bagley.michael@gmail.com>
2016-11-14 17:04:29 -0500

    First pass at JDBC

--- src/main/java/com/inman/Application.java
+++ src/main/java/com/inman/Application.java
@@ -1,16 +1,56 @@
 package com.inman;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import com.inman.model.User;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	private static final Logger log = LoggerFactory.getLogger( Application.class );
+	
     public static void main( String[] args ) {
-	@SuppressWarnings("unused")
-	ApplicationContext ctx = SpringApplication.run( Application.class, args );
+    	
+    	System.out.println( "Spring Boot is like, engaged but why twice?. " );
+    	SpringApplication.run( Application.class, args);
+    }
+    
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    
+    @Override
+    public void run(String... strings) throws Exception {
 
-	System.out.println( "Spring Boot is like, engaged. " );
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE User IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE User(" +
+                "id SERIAL, userName VARCHAR(255), password VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting user record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO user( username, password) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, userName, password FROM user WHERE first_name = ?", new Object[] { "Josh" },
+                (rs, rowNum) -> new User(rs.getLong("id"), rs.getString("userName"), rs.getString("password"))
+        ).forEach(user -> log.info(user.toString()));
     }
 }
 
