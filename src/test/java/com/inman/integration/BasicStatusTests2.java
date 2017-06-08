58f1a416fe0b3e6f0377354e432e1367f08385c8
Michael Bagley <bagley.michael@gmail.com>
2017-06-08 16:10:25 -0400

    Daily checkin

--- src/test/java/com/inman/integration/BasicStatusTests2.java
+++ src/test/java/com/inman/integration/BasicStatusTests2.java
@@ -1,30 +1,28 @@
 package com.inman.integration;
 
-import java.util.Map;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import com.inman.model.rest.StatusResponse;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 @RunWith( SpringRunner.class)
-@SpringBootTest(classes = BasicStatusTests.class )
-public class BasicStatusTests {
+@SpringBootTest(classes = BasicStatusTests2.class )
+public class BasicStatusTests2 {
 
 	private int port= 8080;
 
-	private TestRestTemplate testRestTemplate = new TestRestTemplate();
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Test
 	public void shouldReturnOkAndStatusString() throws Exception {
-		ResponseEntity<StatusResponse> entity = this.testRestTemplate.getForEntity(
+		ResponseEntity<StatusResponse> entity = this.restTemplate.getForEntity(
 				"http://localhost:" + this.port + StatusResponse.rootUrl, StatusResponse.class);
 
 		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
--- src/test/java/com/inman/integration/VerifyCredentials.java
+++ src/test/java/com/inman/integration/VerifyCredentials.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import com.inman.model.rest.StatusResponse;
 import com.inman.model.rest.VerifyCredentialsRequest;
@@ -20,7 +21,7 @@ public class VerifyCredentials {
 
 	private int port= 8080;
 
-	private TestRestTemplate testRestTemplate = new TestRestTemplate();
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Test
 	public void validCredentials() throws Exception {
@@ -29,10 +30,11 @@ public class VerifyCredentials {
 		badCredentials.setUsername("donald");
 		badCredentials.setPassword("trumped");
 		
-		ResponseEntity<VerifyCredentialsResponse> entity = this.testRestTemplate.putForEntity(
+		ResponseEntity<VerifyCredentialsResponse> entity = this.restTemplate.putForEntity(
 				"http://localhost:" + this.port + VerifyCredentialsRequest.rootUrl, VerifyCredentialsResponse.class );
 
 		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 		then(entity.getBody().getStatus().equals( StatusResponse.INMAN_OK ));
 	}
+
 }
